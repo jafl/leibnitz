ecdce0e18aac43b07e2382a4e7f9dca97ffda797
John Lindal <github@newplanetsoftware.com>
2024-02-26 19:07:46 -0800

    fix about dialog

--- code/AboutDialog.cpp
+++ code/AboutDialog.cpp
@@ -72,9 +72,9 @@ AboutDialog::BuildWindow
 	thxIcon->SetImage(GetDisplay()->GetImageCache()->GetImage(thx_about), false);
 
 	auto* textWidget =
-		jnew JXStaticText(JString::empty, window,
+		jnew JXStaticText(JString::empty, true, false, false, nullptr, window,
 					JXWidget::kHElastic, JXWidget::kVElastic, 90,20, 330,110);
-	assert( textWidget != nullptr );
+	textWidget->SetBorderWidth(0);
 
 	auto* npsIcon =
 		jnew JXImageWidget(window,
